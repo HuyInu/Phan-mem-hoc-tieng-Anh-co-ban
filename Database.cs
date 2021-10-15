15a53f8cb4c408d9eda4502fb486aa3be0d48a67
HuyInu <pugasus.na@gmail.com>
2021-10-15 17:10:13 +0700

    Thêm class database
    + class database để kết nối csdl

---
+++ Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Đồ_án
+{
+    class Database
+    {
+        SqlConnection cn;
+        SqlDataAdapter da;
+        DataSet ds;
+
+
+        public Database(string svname, string tendata)
+        {
+            string con = @"Data Source=" + svname + ";Initial Catalog=" + tendata + ";Integrated Security=True";
+            cn = new SqlConnection(con);
+        }
+        public DataTable Execute(string sqlstr)
+        {
+            da = new SqlDataAdapter(sqlstr, cn);
+            ds = new DataSet();
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+        public void ExecuteNonQuery(string ss)
+        {
+            SqlCommand sqlcm = new SqlCommand(ss, cn);
+            cn.Open();
+            sqlcm.ExecuteNonQuery();
+            cn.Close();
+        }
+        public int dem(string ss)
+        {
+            int a;
+            SqlCommand sqlcm = new SqlCommand(ss, cn);
+            cn.Open();
+            a=sqlcm.ExecuteNonQuery();
+            return a;
+        }
+    }
+}
