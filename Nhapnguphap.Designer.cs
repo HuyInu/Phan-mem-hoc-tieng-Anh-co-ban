591ad830aa5efe3959c675245875d92b1802ad7e
NguyenThanhPhuongMinh <92260543+NguyenThanhPhuongMinh@users.noreply.github.com>
2021-10-25 20:38:18 +0700

    nhapnguphap

---
+++ Nhapnguphap.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Đồ_án
+{
+    partial class Nhapnguphap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txthoten = new Đồ_án.RoundTextbox();
+            this.roundTextbox1 = new Đồ_án.RoundTextbox();
+            this.roundTextbox2 = new Đồ_án.RoundTextbox();
+            this.butcapnhat = new Đồ_án.CustomButton();
+            this.butthaotac = new Đồ_án.CustomButton();
+            this.butthem = new Đồ_án.CustomButton();
+            this.butxoa = new Đồ_án.CustomButton();
+            this.dtgNP = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgNP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label1.Location = new System.Drawing.Point(410, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 18);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Nội dung";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label2.Location = new System.Drawing.Point(62, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 18);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Mã BH";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label3.Location = new System.Drawing.Point(62, 40);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Mã NP";
+            // 
+            // txthoten
+            // 
+            this.txthoten.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txthoten.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txthoten.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txthoten.BorderRadius = 15;
+            this.txthoten.BorderSize = 2;
+            this.txthoten.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txthoten.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txthoten.Location = new System.Drawing.Point(496, 33);
+            this.txthoten.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txthoten.Multiline = false;
+            this.txthoten.Name = "txthoten";
+            this.txthoten.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txthoten.PasswordChar = false;
+            this.txthoten.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txthoten.PlaceholderText = "";
+            this.txthoten.Size = new System.Drawing.Size(175, 32);
+            this.txthoten.TabIndex = 26;
+            this.txthoten.Texts = "";
+            this.txthoten.UnderlinedStyle = false;
+            // 
+            // roundTextbox1
+            // 
+            this.roundTextbox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.roundTextbox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.roundTextbox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.roundTextbox1.BorderRadius = 15;
+            this.roundTextbox1.BorderSize = 2;
+            this.roundTextbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundTextbox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.roundTextbox1.Location = new System.Drawing.Point(138, 89);
+            this.roundTextbox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.roundTextbox1.Multiline = false;
+            this.roundTextbox1.Name = "roundTextbox1";
+            this.roundTextbox1.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.roundTextbox1.PasswordChar = false;
+            this.roundTextbox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.roundTextbox1.PlaceholderText = "";
+            this.roundTextbox1.Size = new System.Drawing.Size(175, 32);
+            this.roundTextbox1.TabIndex = 27;
+            this.roundTextbox1.Texts = "";
+            this.roundTextbox1.UnderlinedStyle = false;
+            // 
+            // roundTextbox2
+            // 
+            this.roundTextbox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.roundTextbox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.roundTextbox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.roundTextbox2.BorderRadius = 15;
+            this.roundTextbox2.BorderSize = 2;
+            this.roundTextbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundTextbox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.roundTextbox2.Location = new System.Drawing.Point(138, 33);
+            this.roundTextbox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.roundTextbox2.Multiline = false;
+            this.roundTextbox2.Name = "roundTextbox2";
+            this.roundTextbox2.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.roundTextbox2.PasswordChar = false;
+            this.roundTextbox2.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.roundTextbox2.PlaceholderText = "";
+            this.roundTextbox2.Size = new System.Drawing.Size(175, 32);
+            this.roundTextbox2.TabIndex = 28;
+            this.roundTextbox2.Texts = "";
+            this.roundTextbox2.UnderlinedStyle = false;
+            // 
+            // butcapnhat
+            // 
+            this.butcapnhat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.butcapnhat.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.butcapnhat.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.butcapnhat.BorderRadius = 15;
+            this.butcapnhat.BorderSize = 2;
+            this.butcapnhat.Enabled = false;
+            this.butcapnhat.FlatAppearance.BorderSize = 0;
+            this.butcapnhat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.butcapnhat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.butcapnhat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butcapnhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butcapnhat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.butcapnhat.Location = new System.Drawing.Point(385, 84);
+            this.butcapnhat.Name = "butcapnhat";
+            this.butcapnhat.Size = new System.Drawing.Size(82, 37);
+            this.butcapnhat.TabIndex = 35;
+            this.butcapnhat.Text = "Cập nhật";
+            this.butcapnhat.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.butcapnhat.UseVisualStyleBackColor = false;
+            // 
+            // butthaotac
+            // 
+            this.butthaotac.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.butthaotac.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.butthaotac.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.butthaotac.BorderRadius = 15;
+            this.butthaotac.BorderSize = 2;
+            this.butthaotac.FlatAppearance.BorderSize = 0;
+            this.butthaotac.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.butthaotac.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.butthaotac.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butthaotac.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butthaotac.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.butthaotac.Location = new System.Drawing.Point(496, 84);
+            this.butthaotac.Name = "butthaotac";
+            this.butthaotac.Size = new System.Drawing.Size(82, 37);
+            this.butthaotac.TabIndex = 36;
+            this.butthaotac.Text = "Thao tác";
+            this.butthaotac.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.butthaotac.UseVisualStyleBackColor = false;
+            // 
+            // butthem
+            // 
+            this.butthem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.butthem.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.butthem.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.butthem.BorderRadius = 15;
+            this.butthem.BorderSize = 2;
+            this.butthem.Enabled = false;
+            this.butthem.FlatAppearance.BorderSize = 0;
+            this.butthem.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.butthem.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.butthem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butthem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butthem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.butthem.Location = new System.Drawing.Point(605, 84);
+            this.butthem.Name = "butthem";
+            this.butthem.Size = new System.Drawing.Size(82, 37);
+            this.butthem.TabIndex = 37;
+            this.butthem.Text = "Thêm";
+            this.butthem.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.butthem.UseVisualStyleBackColor = false;
+            // 
+            // butxoa
+            // 
+            this.butxoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(166)))), ((int)(((byte)(174)))));
+            this.butxoa.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(166)))), ((int)(((byte)(174)))));
+            this.butxoa.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(20)))), ((int)(((byte)(41)))));
+            this.butxoa.BorderRadius = 15;
+            this.butxoa.BorderSize = 2;
+            this.butxoa.Enabled = false;
+            this.butxoa.FlatAppearance.BorderSize = 0;
+            this.butxoa.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(203)))), ((int)(((byte)(0)))));
+            this.butxoa.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(247)))), ((int)(((byte)(13)))));
+            this.butxoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butxoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butxoa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(0)))), ((int)(((byte)(16)))));
+            this.butxoa.Location = new System.Drawing.Point(496, 138);
+            this.butxoa.Name = "butxoa";
+            this.butxoa.Size = new System.Drawing.Size(82, 37);
+            this.butxoa.TabIndex = 38;
+            this.butxoa.Text = "Xóa";
+            this.butxoa.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(0)))), ((int)(((byte)(16)))));
+            this.butxoa.UseVisualStyleBackColor = false;
+            // 
+            // dtgNP
+            // 
+            this.dtgNP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgNP.Location = new System.Drawing.Point(65, 221);
+            this.dtgNP.Name = "dtgNP";
+            this.dtgNP.Size = new System.Drawing.Size(622, 150);
+            this.dtgNP.TabIndex = 39;
+            // 
+            // Nhapnguphap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 398);
+            this.Controls.Add(this.dtgNP);
+            this.Controls.Add(this.butxoa);
+            this.Controls.Add(this.butthem);
+            this.Controls.Add(this.butthaotac);
+            this.Controls.Add(this.butcapnhat);
+            this.Controls.Add(this.roundTextbox2);
+            this.Controls.Add(this.roundTextbox1);
+            this.Controls.Add(this.txthoten);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Nhapnguphap";
+            this.Text = "Nhapnguphap";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgNP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private RoundTextbox txthoten;
+        private RoundTextbox roundTextbox1;
+        private RoundTextbox roundTextbox2;
+        private CustomButton butcapnhat;
+        private CustomButton butthaotac;
+        private CustomButton butthem;
+        private CustomButton butxoa;
+        private System.Windows.Forms.DataGridView dtgNP;
+    }
+}
