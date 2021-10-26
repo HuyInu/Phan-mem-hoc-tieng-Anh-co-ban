8d40b9431dfde54eb7d2a179dfa0326b0043340f
HuyInu <pugasus.na@gmail.com>
2021-10-26 12:02:50 +0700

    update 0.2
    - Gộp nhập TV vs NP thành 1
    -Thêm cột CSDL

--- QLAV.sql
+++ QLAV.sql
@@ -26,8 +26,12 @@ create table TUVUNG(
 	MaTV char(10) not null,
 	MaBH int not null,
 	Noidung varchar(20),
-	Loai char(5)
 	constraint pk_TUVUNG primary key(MaTV) 
+)
+create table LOAITUVUNG(
+	MaLOAI char(5) not null,
+	TenLoai varchar(10)
+	constraint pk_LOAITUVUNG primary key(MaLOAI)
 )
 create table NGUPHAP(
 	MaNP int not null,
@@ -46,7 +50,7 @@ create table CAUHOI(
 	constraint pk_CAUHOI primary key(MaCH) 
 )
 create table KIEMTRA(
-	MaCH char(1) not null,
+	MaCH char(5) not null,
 	MaNH int not null,
 	constraint pk_KIEMTRA primary key(MaCH,MaNH) 
 )
@@ -55,7 +59,7 @@ ALTER TABLE TUVUNG  ADD CONSTRAINT FK01_TV FOREIGN KEY(MaBH) REFERENCES BAIHOC(M
 
 ALTER TABLE NGUPHAP ADD CONSTRAINT FK01_NP FOREIGN KEY(MaBH) REFERENCES BAIHOC(MaBH) on delete cascade
 /*ALTER TABLE NGUPHAP DROP CONSTRAINT FK01_NP*/
-
+ALTER TABLE TUVUNG  ADD CONSTRAINT FK01_TV FOREIGN KEY(MaLOAI) REFERENCES LOAITUVUNG(MaLOAI) on delete cascade
 
 /*insert into NGUOIHOC values(4,'ia','ssd','aasdsd','aggaaa',165,4456)*/
 
@@ -65,11 +69,15 @@ select * from BAIHOC
 select * from CAUHOI
 select * from NGUOIHOC
 select * from TUVUNG
+select * from LOAITUVUNG
 
 /*delete from BAIHOC
 delete from TUVUNG
 
 insert into TUVUNG values('V1',2,'Hello','V')
 insert into BAIHOC values (2,'bai2')*/
-alter table TUVUNG add TiengViet char(20)
-//
+alter table TUVUNG add MaLOAI char(5) not null
+
+ALTER TABLE KIEMTRA ALTER COLUMN MaCH char(5)
+
+ALTER TABLE TUVUNG DROP COLUMN Loai
