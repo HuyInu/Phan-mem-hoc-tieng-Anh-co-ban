ee94ee9ab271e2b09cc98635df281d00478ba858
NguyenThanhPhuongMinh <92260543+NguyenThanhPhuongMinh@users.noreply.github.com>
2021-10-29 15:52:52 +0700

    ww

--- Formnguoihoc/Thongtincanhan/formcanhan.Designer.cs
+++ Formnguoihoc/Thongtincanhan/formcanhan.Designer.cs
@@ -33,10 +33,17 @@
             this.pnelleft = new Guna.UI2.WinForms.Guna2Panel();
             this.pnelava = new System.Windows.Forms.Panel();
             this.maincrn = new System.Windows.Forms.Panel();
-            this.labhoten = new System.Windows.Forms.Label();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtkinhnghiem = new Đồ_án.RoundTextbox();
+            this.txtcapdo = new Đồ_án.RoundTextbox();
+            this.txtemail = new Đồ_án.RoundTextbox();
+            this.txthoten = new Đồ_án.RoundTextbox();
             this.gradian1.SuspendLayout();
             this.pnelleft.SuspendLayout();
             this.maincrn.SuspendLayout();
@@ -51,9 +58,9 @@
             this.gradian1.Controls.Add(this.maincrn);
             this.gradian1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gradian1.Location = new System.Drawing.Point(0, 0);
-            this.gradian1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gradian1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gradian1.Name = "gradian1";
-            this.gradian1.Size = new System.Drawing.Size(1284, 702);
+            this.gradian1.Size = new System.Drawing.Size(963, 570);
             this.gradian1.TabIndex = 4;
             // 
             // pnelleft
@@ -62,19 +69,18 @@
             this.pnelleft.Controls.Add(this.pnelava);
             this.pnelleft.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnelleft.Location = new System.Drawing.Point(0, 0);
-            this.pnelleft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnelleft.Name = "pnelleft";
             this.pnelleft.ShadowDecoration.Parent = this.pnelleft;
-            this.pnelleft.Size = new System.Drawing.Size(252, 702);
+            this.pnelleft.Size = new System.Drawing.Size(189, 570);
             this.pnelleft.TabIndex = 5;
             // 
             // pnelava
             // 
             this.pnelava.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
-            this.pnelava.Location = new System.Drawing.Point(15, 49);
-            this.pnelava.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnelava.Location = new System.Drawing.Point(11, 40);
+            this.pnelava.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnelava.Name = "pnelava";
-            this.pnelava.Size = new System.Drawing.Size(235, 148);
+            this.pnelava.Size = new System.Drawing.Size(176, 120);
             this.pnelava.TabIndex = 0;
             // 
             // maincrn
@@ -84,24 +90,32 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.maincrn.AutoScroll = true;
             this.maincrn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.maincrn.Controls.Add(this.txthoten);
+            this.maincrn.Controls.Add(this.txtemail);
+            this.maincrn.Controls.Add(this.txtcapdo);
+            this.maincrn.Controls.Add(this.txtkinhnghiem);
+            this.maincrn.Controls.Add(this.label4);
+            this.maincrn.Controls.Add(this.label3);
+            this.maincrn.Controls.Add(this.label2);
+            this.maincrn.Controls.Add(this.label1);
             this.maincrn.Controls.Add(this.guna2CirclePictureBox1);
-            this.maincrn.Controls.Add(this.labhoten);
-            this.maincrn.Location = new System.Drawing.Point(259, 49);
-            this.maincrn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.maincrn.Location = new System.Drawing.Point(194, 40);
+            this.maincrn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.maincrn.Name = "maincrn";
-            this.maincrn.Size = new System.Drawing.Size(972, 606);
+            this.maincrn.Size = new System.Drawing.Size(729, 492);
             this.maincrn.TabIndex = 4;
             // 
-            // labhoten
+            // guna2CirclePictureBox1
             // 
-            this.labhoten.AutoSize = true;
-            this.labhoten.Font = new System.Drawing.Font("HL-Comic1unicode", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labhoten.Location = new System.Drawing.Point(296, 209);
-            this.labhoten.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labhoten.Name = "labhoten";
-            this.labhoten.Size = new System.Drawing.Size(74, 20);
-            this.labhoten.TabIndex = 1;
-            this.labhoten.Text = "Họ tên";
+            this.guna2CirclePictureBox1.ImageRotate = 0F;
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(43, 11);
+            this.guna2CirclePictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(174, 176);
+            this.guna2CirclePictureBox1.TabIndex = 2;
+            this.guna2CirclePictureBox1.TabStop = false;
             // 
             // guna2Elipse1
             // 
@@ -113,24 +127,148 @@
             this.guna2Elipse2.BorderRadius = 25;
             this.guna2Elipse2.TargetControl = this.pnelava;
             // 
-            // guna2CirclePictureBox1
+            // label1
             // 
-            this.guna2CirclePictureBox1.ImageRotate = 0F;
-            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(57, 13);
-            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
-            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
-            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(232, 216);
-            this.guna2CirclePictureBox1.TabIndex = 2;
-            this.guna2CirclePictureBox1.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label1.Location = new System.Drawing.Point(275, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Họ tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label2.Location = new System.Drawing.Point(275, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 18);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label3.Location = new System.Drawing.Point(242, 203);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 18);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Kinh nghiệm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label4.Location = new System.Drawing.Point(275, 151);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 18);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Cấp độ";
+            // 
+            // txtkinhnghiem
+            // 
+            this.txtkinhnghiem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txtkinhnghiem.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txtkinhnghiem.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txtkinhnghiem.BorderRadius = 15;
+            this.txtkinhnghiem.BorderSize = 2;
+            this.txtkinhnghiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkinhnghiem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txtkinhnghiem.Location = new System.Drawing.Point(353, 196);
+            this.txtkinhnghiem.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtkinhnghiem.Multiline = false;
+            this.txtkinhnghiem.Name = "txtkinhnghiem";
+            this.txtkinhnghiem.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txtkinhnghiem.PasswordChar = false;
+            this.txtkinhnghiem.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtkinhnghiem.PlaceholderText = "";
+            this.txtkinhnghiem.Size = new System.Drawing.Size(175, 32);
+            this.txtkinhnghiem.TabIndex = 24;
+            this.txtkinhnghiem.Texts = "";
+            this.txtkinhnghiem.UnderlinedStyle = false;
+            // 
+            // txtcapdo
+            // 
+            this.txtcapdo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txtcapdo.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txtcapdo.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txtcapdo.BorderRadius = 15;
+            this.txtcapdo.BorderSize = 2;
+            this.txtcapdo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcapdo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txtcapdo.Location = new System.Drawing.Point(353, 144);
+            this.txtcapdo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtcapdo.Multiline = false;
+            this.txtcapdo.Name = "txtcapdo";
+            this.txtcapdo.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txtcapdo.PasswordChar = false;
+            this.txtcapdo.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtcapdo.PlaceholderText = "";
+            this.txtcapdo.Size = new System.Drawing.Size(175, 32);
+            this.txtcapdo.TabIndex = 25;
+            this.txtcapdo.Texts = "";
+            this.txtcapdo.UnderlinedStyle = false;
+            // 
+            // txtemail
+            // 
+            this.txtemail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txtemail.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txtemail.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txtemail.BorderRadius = 15;
+            this.txtemail.BorderSize = 2;
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txtemail.Location = new System.Drawing.Point(353, 98);
+            this.txtemail.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtemail.Multiline = false;
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txtemail.PasswordChar = false;
+            this.txtemail.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtemail.PlaceholderText = "";
+            this.txtemail.Size = new System.Drawing.Size(175, 32);
+            this.txtemail.TabIndex = 26;
+            this.txtemail.Texts = "";
+            this.txtemail.UnderlinedStyle = false;
+            // 
+            // txthoten
+            // 
+            this.txthoten.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txthoten.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txthoten.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txthoten.BorderRadius = 15;
+            this.txthoten.BorderSize = 2;
+            this.txthoten.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txthoten.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txthoten.Location = new System.Drawing.Point(353, 49);
+            this.txthoten.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txthoten.Multiline = false;
+            this.txthoten.Name = "txthoten";
+            this.txthoten.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txthoten.PasswordChar = false;
+            this.txthoten.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txthoten.PlaceholderText = "";
+            this.txthoten.Size = new System.Drawing.Size(175, 32);
+            this.txthoten.TabIndex = 27;
+            this.txthoten.Texts = "";
+            this.txthoten.UnderlinedStyle = false;
             // 
             // formcanhan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1284, 702);
+            this.ClientSize = new System.Drawing.Size(963, 570);
             this.Controls.Add(this.gradian1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "formcanhan";
             this.Text = "formcanhan";
             this.gradian1.ResumeLayout(false);
@@ -147,9 +285,16 @@
         private Guna.UI2.WinForms.Guna2Panel pnelleft;
         private System.Windows.Forms.Panel pnelava;
         private System.Windows.Forms.Panel maincrn;
-        private System.Windows.Forms.Label labhoten;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
         private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private RoundTextbox txtkinhnghiem;
+        private RoundTextbox txthoten;
+        private RoundTextbox txtemail;
+        private RoundTextbox txtcapdo;
     }
 }
---
+++ Nhapkiemtra.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Đồ_án
+{
+    partial class Nhapkiemtra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmaCH = new Đồ_án.RoundTextbox();
+            this.txtmaNH = new Đồ_án.RoundTextbox();
+            this.dtgnhapKT = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgnhapKT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label1.Location = new System.Drawing.Point(36, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Mã NH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.label2.Location = new System.Drawing.Point(436, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 18);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Mã CH";
+            // 
+            // txtmaCH
+            // 
+            this.txtmaCH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txtmaCH.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txtmaCH.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txtmaCH.BorderRadius = 15;
+            this.txtmaCH.BorderSize = 2;
+            this.txtmaCH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmaCH.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txtmaCH.Location = new System.Drawing.Point(494, 40);
+            this.txtmaCH.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtmaCH.Multiline = false;
+            this.txtmaCH.Name = "txtmaCH";
+            this.txtmaCH.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txtmaCH.PasswordChar = false;
+            this.txtmaCH.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtmaCH.PlaceholderText = "";
+            this.txtmaCH.Size = new System.Drawing.Size(175, 32);
+            this.txtmaCH.TabIndex = 22;
+            this.txtmaCH.Texts = "";
+            this.txtmaCH.UnderlinedStyle = false;
+            // 
+            // txtmaNH
+            // 
+            this.txtmaNH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
+            this.txtmaNH.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(179)))), ((int)(((byte)(255)))));
+            this.txtmaNH.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.txtmaNH.BorderRadius = 15;
+            this.txtmaNH.BorderSize = 2;
+            this.txtmaNH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmaNH.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
+            this.txtmaNH.Location = new System.Drawing.Point(94, 40);
+            this.txtmaNH.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtmaNH.Multiline = false;
+            this.txtmaNH.Name = "txtmaNH";
+            this.txtmaNH.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.txtmaNH.PasswordChar = false;
+            this.txtmaNH.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.txtmaNH.PlaceholderText = "";
+            this.txtmaNH.Size = new System.Drawing.Size(175, 32);
+            this.txtmaNH.TabIndex = 23;
+            this.txtmaNH.Texts = "";
+            this.txtmaNH.UnderlinedStyle = false;
+            // 
+            // dtgnhapKT
+            // 
+            this.dtgnhapKT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgnhapKT.Location = new System.Drawing.Point(66, 133);
+            this.dtgnhapKT.Name = "dtgnhapKT";
+            this.dtgnhapKT.Size = new System.Drawing.Size(585, 194);
+            this.dtgnhapKT.TabIndex = 24;
+            // 
+            // Nhapkiemtra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 359);
+            this.Controls.Add(this.dtgnhapKT);
+            this.Controls.Add(this.txtmaNH);
+            this.Controls.Add(this.txtmaCH);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Nhapkiemtra";
+            this.Text = "Nhapkiemtra";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgnhapKT)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private RoundTextbox txtmaCH;
+        private RoundTextbox txtmaNH;
+        private System.Windows.Forms.DataGridView dtgnhapKT;
+    }
+}
